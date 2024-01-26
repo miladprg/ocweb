0e3c785f4039fe15d68b2f18b1372eb03ef4b023
miladprg <115424216+miladprg@users.noreply.github.com>
2024-01-26 10:49:28 +0330

    Create manage.sh

---
+++ manage.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+OCPASSWD_PATH="/etc/ocserv/"
+OCPASSWD="${OCPASSWD_PATH}ocpasswd"
+
+add_user(){
+	echo -e "$2\n$2" | ocpasswd -c $OCPASSWD $1
+}
+
+delete_user(){
+        ocpasswd -d $1
+}
+
+change_user_group(){
+	sed -i "s/$1:.*:/$1:$2:/g" $OCPASSWD
+}
+
+lock_user() {
+	ocpasswd -l $1
+}
+
+unlock_user() {
+        ocpasswd -u $1
+}
+
+create_group() {
+	mkdir -p /etc/ocserv/group
+	file_path="/etc/ocserv/group/g$1"
+	if [ ! -e "$file_path" ]; then
+		echo "max-same-clients = $1" > $file_path
+	fi
+}
+
+flush_ocpasswd() {
+	rm $OCPASSWD
+	touch $OCPASSWD
+}
+
+flush_groups() {
+  rm -rf /etc/ocserv/group
+}
+
+show_online_users() {
+  occtl show users
+}
+
+disconnect_user() {
+  occtl disconnect user $1
+}
+
+$@
